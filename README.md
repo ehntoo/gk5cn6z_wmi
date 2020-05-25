2cffdc82cd04195d806ed1964151fcc431dd482c
Mitchell Johnson <ehntoo@ehntoo.org>
2020-05-25 18:35:15 -0400

    More readme links, add event guid test in mod load hook

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+https://github.com/mkottman/acpi_call
+
 https://github.com/pali/bmfdec
 
 https://github.com/iksaif/wmidump
 
 https://lwn.net/Articles/391230/
+
+https://tldp.org/LDP/lkmpg/2.6/html/index.html
+
+http://derekmolloy.ie/writing-a-linux-kernel-module-part-1-introduction/
--- gk5cn6z_wmi.c
+++ gk5cn6z_wmi.c
@@ -1,17 +1,24 @@
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/init.h>		/* Needed for the macros */
-#define DRIVER_AUTHOR "Mitchell Johnson <ehntoo@ehntoo.org>"
-#define DRIVER_DESC   "GK5CN6Z WMI Driver"
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/acpi.h>
+
+MODULE_AUTHOR("Mitchell Johnson <ehntoo@ehntoo.org>");
+MODULE_DESCRIPTION("GK5CN6Z WMI Driver");
+MODULE_LICENSE("Dual MIT/GPL");
 
 #define GK5CN6Z_ECRAM_GUID      "ABBC0F6F-8EA1-11D1-00A0-C90629100000"
 #define GK5CN6Z_ECRAM_METHOD_ID 4
 
-#define GK5CN6Z_EVENT_GUID      "ABBC0F72-8EA1-11d1-00A0-C90629100000"
+#define GK5CN6Z_EVENT_GUID      "ABBC0F72-8EA1-11D1-00A0-C90629100000"
 
 static int __init init_hello_4(void)
 {
 	printk(KERN_INFO "Hello, world 4\n");
+  if (!wmi_has_guid(GK5CN6Z_EVENT_GUID)) {
+	    pr_warn("No known WMI event GUID found\n");
+	    return -ENODEV;
+	}
 	return 0;
 }
 
@@ -22,25 +29,3 @@ static void __exit cleanup_hello_4(void)
 
 module_init(init_hello_4);
 module_exit(cleanup_hello_4);
-
-/*  
- *  You can use strings, like this:
- */
-
-/* 
- * Get rid of taint message by declaring code as GPL. 
- */
-MODULE_LICENSE("GPL");
-
-/*
- * Or with defines, like this:
- */
-MODULE_AUTHOR(DRIVER_AUTHOR);	/* Who wrote this module? */
-MODULE_DESCRIPTION(DRIVER_DESC);	/* What does this module do */
-
-/*  
- *  This module uses /dev/testdevice.  The MODULE_SUPPORTED_DEVICE macro might
- *  be used in the future to help automatic configuration of modules, but is 
- *  currently unused other than for documentation purposes.
- */
-MODULE_SUPPORTED_DEVICE("testdevice");
